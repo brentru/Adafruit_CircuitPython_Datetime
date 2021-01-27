680798ad7ba2ee414ee13cb984b0ed5df1d8282e
brentru <robots199@me.com>
2021-01-27 14:36:00 -0500

    license and copyright examples

--- examples/datetime_simpletest.py
+++ examples/datetime_simpletest.py
@@ -1,3 +1,13 @@
+# SPDX-FileCopyrightText: 2001-2021 Python Software Foundation.All rights reserved.
+# SPDX-FileCopyrightText: 2000 BeOpen.com. All rights reserved.
+# SPDX-FileCopyrightText: 1995-2001 Corporation for National Research Initiatives.
+#                         All rights reserved.
+# SPDX-FileCopyrightText: 1995-2001 Corporation for National Research Initiatives.
+#                         All rights reserved.
+# SPDX-FileCopyrightText: 1991-1995 Stichting Mathematisch Centrum. All rights reserved.
+# SPDX-FileCopyrightText: 2021 Brent Rubell for Adafruit Industries
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 # Example of working with a `datetime` object
 # from https://docs.python.org/3/library/datetime.html#examples-of-usage-datetime
 from adafruit_datetime import datetime, date, time, timezone
