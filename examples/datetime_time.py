27d8e599fbe683e016fc05c179b4bbf96d9aa503
brentru <robots199@me.com>
2021-01-27 13:09:35 -0500

    init and cut!

---
+++ examples/datetime_time.py
@@ -0,0 +1,20 @@
+# Example of working with a `time` object
+# from https://docs.python.org/3/library/datetime.html#examples-of-usage-time
+from adafruit_datetime import time, timezone
+
+# Create a new time object
+t = time(12, 10, 30, tzinfo=timezone.utc)
+
+# ISO 8601 formatted string
+iso_time = t.isoformat()
+print('ISO8601-Formatted Time:', iso_time)
+
+# Timezone name
+print("Timezone Name:", t.tzname())
+
+# Return a string representing the time, controlled by an explicit format string
+strf_time = t.strftime("%H:%M:%S %Z")
+print('Formatted time string:', strf_time)
+
+# Specifies a format string in formatted string literals
+print('The time is {:%H:%M}.'.format(t))
---
+++ examples/datetime_timedelta.py
@@ -0,0 +1,20 @@
+# Example of working with a `timedelta` object
+# from https://docs.python.org/3/library/datetime.html#examples-of-usage-timedelta
+from adafruit_datetime import timedelta
+
+# Example of normalization
+year = timedelta(days=365)
+another_year = timedelta(weeks=40, days=84, hours=23,
+                         minutes=50, seconds=600)
+print("Total seconds in the year: ", year.total_seconds())
+
+# Example of timedelta arithmetic
+year = timedelta(days=365)
+ten_years = 10 * year
+print('Days in ten years:', ten_years)
+
+nine_years = ten_years - year
+print('Days in nine years:', nine_years)
+
+three_years = nine_years // 3
+print('Days in three years:', three_years, three_years.days // 365)
